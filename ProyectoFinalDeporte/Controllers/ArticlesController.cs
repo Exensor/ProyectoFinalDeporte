2f537e5131708fd57cb2b77422db1129f6b09ee5
Iván <ivanfr89@gmail.com>
2017-05-21 15:32:19 +0200

    Creación de las noticias y su personalización. Create, Update y Delete solo para administradores

---
+++ ProyectoFinalDeporte/Controllers/ArticlesController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoFinalDeporte.Models;
+using Microsoft.AspNet.Identity;
+
+namespace ProyectoFinalDeporte.Controllers
+{
+    [Authorize]
+    public class ArticlesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Articles
+        public ActionResult Index()
+        {
+
+            return View(db.Articles.ToList());
+        }
+
+        // GET: Articles/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Article article = db.Articles.Find(id);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            return View(article);
+        }
+
+        // GET: Articles/Create
+        public ActionResult Create()
+        {
+            string currentUserId = User.Identity.GetUserId();
+            var userAdmin = db.Admins.Where(p => p.IdUser == currentUserId);
+
+            if (userAdmin.Count() == 0)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        // POST: Articles/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ArticleId,Title,Content")] Article article)
+        {
+            string currentUserId = User.Identity.GetUserId();
+            var userAdmin = db.Admins.Where(p => p.IdUser == currentUserId);
+
+            if (userAdmin.Count() == 0)
+            {
+                return RedirectToAction("Index");
+            }
+
+            if (ModelState.IsValid)
+            {
+                db.Articles.Add(article);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(article);
+        }
+
+        // GET: Articles/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            string currentUserId = User.Identity.GetUserId();
+            var userAdmin = db.Admins.Where(p => p.IdUser == currentUserId);
+
+            if (userAdmin.Count() == 0)
+            {
+                return RedirectToAction("Index");
+            }
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Article article = db.Articles.Find(id);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            return View(article);
+        }
+
+        // POST: Articles/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ArticleId,Title,Content")] Article article)
+        {
+            string currentUserId = User.Identity.GetUserId();
+            var userAdmin = db.Admins.Where(p => p.IdUser == currentUserId);
+
+            if (userAdmin.Count() == 0)
+            {
+                return RedirectToAction("Index");
+            }
+            if (ModelState.IsValid)
+            {
+                db.Entry(article).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(article);
+        }
+
+        // GET: Articles/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            string currentUserId = User.Identity.GetUserId();
+            var userAdmin = db.Admins.Where(p => p.IdUser == currentUserId);
+
+            if (userAdmin.Count() == 0)
+            {
+                return RedirectToAction("Index");
+            }
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Article article = db.Articles.Find(id);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            return View(article);
+        }
+
+        // POST: Articles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            string currentUserId = User.Identity.GetUserId();
+            var userAdmin = db.Admins.Where(p => p.IdUser == currentUserId);
+
+            if (userAdmin.Count() == 0)
+            {
+                return RedirectToAction("Index");
+            }
+            Article article = db.Articles.Find(id);
+            db.Articles.Remove(article);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
